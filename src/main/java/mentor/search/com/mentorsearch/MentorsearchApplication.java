19b49487885701b724cfda961e14c2694e6b1ac2
wlad267 <st.ghita@gmail.com>
2018-12-12 10:21:11 +0200

    initial share

---
+++ src/main/java/mentor/search/com/mentorsearch/MentorsearchApplication.java
@@ -0,0 +1,12 @@
+package mentor.search.com.mentorsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorsearchApplication.class, args);
+	}
+}
